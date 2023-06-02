9aa3edef0ede485a64f44c788f9d7f257a0020dd
dani miletic <danimiletic@gmail.com>
2023-06-02 12:47:15 -0600

    fixed bug where param was throwing error in reviews destroy method. All backend core functionality set and working. Tested in Insomnia

---
+++ app/controllers/api/v1/albums_controller.rb
@@ -0,0 +1,62 @@
+module Api
+  module V1
+    class AlbumsController < ApplicationController
+
+      # avoids authenticity token error
+      protect_from_forgery with: :null_session
+
+      def index 
+        albums = Album.all
+        
+        render json: AlbumSerializer.new(albums, options).serialized_json
+      end
+
+      def show
+        album = Album.find_by(slug: params[:slug])
+
+        render json: AlbumSerializer.new(album, options).serialized_json
+      end
+
+      def create
+        album =Album.new(album_params)
+
+        if album.save
+          render json: AlbumSerializer.new(album).serialized_json
+        else 
+          render json: {error: album.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        album =Album.find_by(slug: params[:slug])
+        if album.update(album_params)
+          render json: AlbumSerializer.new(album, options).serialized_json
+        else 
+          render json: {error: album.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        album = Album.find_by(slug: params[:slug])
+
+        if album.destroy
+          head :no_content
+        else 
+          render json: {error: album.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def album_params
+        params.require(:album).permit(:name, :artist, :slug, :image_url)
+      end
+
+      def options
+
+        #creates "compound document" displays the reviews associated 
+        @options ||= { include: %i[reviews] }
+      end
+    end
+  end
+end
